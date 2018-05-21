ea9e8d9eb61d8f1c55d8ef22c7bc6e098df8519d
Tom Reese III <tr3limited@gmail.com>
2018-05-21 08:41:17 -0500

    css_exercise_updates
    algorithm exercises 4 and 5...done
    simply responsive....done
    responsive blocks....not done
    bootstrap...not done

---
+++ Web_Fundamentals/CSS/algorithm_4/test.js
@@ -0,0 +1,84 @@
+// Given an array and a value Y, count and print the number of array values greater than Y.
+
+function greaterY(arr, y){
+  var i = 0;
+  var sum = 0;
+  var l = arr.length;
+  while(i < l){
+    if(arr[i] > y){
+      sum++;
+      i++;
+    }
+    else{
+      i++;
+    }
+  }
+  console.log(sum);
+}
+
+greaterY([2,3,4,5,6],2);
+greaterY([2,3,-6],0);
+
+// Given an array, print the max, min and average values for that array.
+
+function maxMinAvg(arr){
+var max = arr[0];
+var min = arr[0];
+var sum = 0;
+var i = 0;
+var j = arr.length;
+while(i< j){
+  if (arr[i] > max){
+    max = arr[i];
+  }
+  if(arr[i] < min){
+    min = arr[i];
+  }
+  sum+=arr[i];
+  i++;
+}
+var avg = sum/j;
+var arr = [max, min, avg];
+console.log(arr);
+}
+
+maxMinAvg([1,2,3,4,5,6,7,8,9]);
+maxMinAvg([5,5,5,5]);
+maxMinAvg([-1,-1,7,7]);
+maxMinAvg([8,-1,7,7]);
+
+// Given an array of numbers, create a function that returns a new array where negative values were replaced with the string ‘Dojo’.   For example, replaceNegatives( [1,2,-3,-5,5]) should return [1,2, "Dojo", "Dojo", 5].
+
+function dojoN(x){
+  var i = 0;
+  var j = x.length;
+  while(i < j){
+    if(x[i] < 0){
+      x[i] = "Dojo";
+    }
+    i++;
+  }
+  console.log(x);
+}
+
+dojoN([-1,3,-1,4]);
+dojoN([-1,3,-1,0,-99999,0.5]);
+
+// Given array, and indices start and end, remove vals in that index range, working in-place (hence shortening the array).  For example, removeVals([20,30,40,50,60,70],2,4) should return [20,30,70].
+
+function removeVals(x,a,b){
+  var q = [];
+  var w = x[a];
+  var e = x[b];
+  for(var i = 0; i < x.length; i++){
+   if(x[i] >= w && x[i] <= e){
+     continue;
+   }
+   else{
+     q.push(x[i]);
+   }
+   console.log(q);
+ }
+}
+
+removeVals([20,30,40,50,60,70],2,4);
---
+++ Web_Fundamentals/CSS/algorithm_5/test.js
@@ -0,0 +1,90 @@
+// Return the given array, after setting any negative values to zero.  For example resetNegatives( [1,2,-1, -3]) should return [1,2,0,0].
+
+function noNEg(x) {
+  var i = 0;
+  var j = x.length;
+  while (i<j) {
+    if(x[i] < 0){
+      x[i] = 0;
+      i++;
+    }
+    else{
+      i++;
+      continue;
+    }
+  }
+  console.log(x);
+}
+
+noNEg([0,-1,3,4,-2,-999,998]);
+
+
+// Given an array, move all values forward by one index, dropping the first and leaving a ‘0’ value at the end.  For example moveForward( [1,2,3]) should return [2,3,0].
+
+
+function moveOver(x){
+x.shift();
+x.push(0);
+console.log(x);
+}
+moveOver([1,2,3]);
+
+// Given an array, return an array with values in a reversed order.  For example, returnReversed([1,2,3]) should return [3,2,1].
+
+
+function revOrd(x){
+x.reverse(x);
+console.log(x);
+}
+
+revOrd([1,2,3,4,5]);
+
+// function revOrd2(b){
+//   var p = [];
+//   var i = 0;
+//   var l = -1
+//   var sum = 0;
+//   var j = b.length;
+//   while (i<j) {
+//     p.push(j+l)
+//     l=l+(l);
+//     sum++;
+//   }
+//   console.log(p);
+// }
+// revOrd2([1,2,3,4,5]);
+// Create a function that changes a given array to list each original element twice, retaining original order.  Have the function return the new array.  For example repeatTwice( [4,”Ulysses”, 42, false] ) should return [4,4, “Ulysses”, “Ulysses”, 42, 42, false, false].
+
+function twiceArr(x){
+  var p = [];
+  var i = 0;
+  var j = x.length;
+  while (i<j){
+    p.push(x[i]);
+    p.push(x[i]);
+    i++;
+  }
+  console.log(p);
+}
+
+twiceArr([1,2,3, "done"]);
+
+// Given an array and a value Y, count and print the number of array values greater than Y.
+
+function greaterY(x,y){
+  var i = 0;
+  var j = x.length;
+  var sum = 0;
+  while(i<j){
+    if(x[i] > y){
+      sum++;
+      i++;
+    }
+    else{
+      i++;
+    }
+  }
+  console.log(sum);
+}
+
+greaterY([0,-5,2,5,6,7,8,4,3,7,1,2,0],3);
