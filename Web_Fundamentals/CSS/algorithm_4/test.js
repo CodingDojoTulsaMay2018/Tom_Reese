5c6e388740a13f73e956221248f4b3d1dbd6e034
Tom Reese III <tr3limited@gmail.com>
2018-05-25 15:35:52 -0500

    mysql_update
    mysql workbench setup finished, updating other exercise files as well.

--- Web_Fundamentals/CSS/algorithm_4/test.js
+++ Web_Fundamentals/CSS/algorithm_4/test.js
@@ -66,20 +66,46 @@ dojoN([-1,3,-1,0,-99999,0.5]);
 
 // Given array, and indices start and end, remove vals in that index range, working in-place (hence shortening the array).  For example, removeVals([20,30,40,50,60,70],2,4) should return [20,30,70].
 
-function removeVals(x,a,b){
-  var q = [];
-  var w = x[a];
-  var e = x[b];
-  for(var i = 0; i < x.length; i++){
-   if(x[i] >= w && x[i] <= e){
-     continue;
-   }
-   else{
-     q.push(x[i]);
-   }
-   console.log(q);
- }
-}
+// function removeVals(x,a,b){
+//   if(x.length-1 < a || x.length-1 < b){
+//     console.log ("null");
+//     return;
+//   }
+//   if(a>b){
+//     console.log("invalid input, first parameter must be smaller than second parameter")
+//     return;
+//   }
+//   var q = [];
+//   var w = x[a];
+//   var e = x[b];
+//   for(var i = 0; i < x.length; i++){
+//    if(x[i] >= w && x[i] <= e || x[i] >= e && x[i] <= w){
+//      continue;
+//    }
+//    else{
+//      q.push(x[i]);
+//    }
+//  }
+//  console.log(q);
+// }
 
+// function splice(arr, lowIdx, highExc) { 
+//   if (lowIdx < 0 || highExc > arr.length || highExc <= lowIdx || (highExc - lowIdx) > arr.length) 
+//   {return null; }
+//   else { 
+//     for (var i = lowIdx, j = 0; i < highExc; i++, j++) {
+//       arr[j] = arr[i]; }
+//       arr.length = highExc - lowIdx; return arr}; }
 
-removeVals([20,30,40,50,60,70],2,4);
+console.log(removeInner([0,1,2,3,4,5],2,4));
+
+function removeInner(x, a,b) {
+  if (a < 0 || b > x.length - 1 || (b - a) >= x.length - 1) {
+      return null;
+    } 
+  else {
+    for (var i = a, j = b + 1; j < x.length; i++, j++) {
+      console.log(j,a,x)  ;
+      x[i] = x[j];} 
+        x.length -= b - a + 1;} 
+return x; }
