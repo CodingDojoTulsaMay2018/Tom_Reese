6ab72dab9d4b183eae0a8ebf61906c910609faa4
Tom Reese III <tr3limited@gmail.com>
2018-06-02 12:49:17 -0500

    API_and_AJAX
    exercises updated

--- Web_Fundamentals/APIs_and_AJAX/pokemon/main.js
+++ Web_Fundamentals/APIs_and_AJAX/pokemon/main.js
@@ -1,13 +1,44 @@
 $(document).ready(function(){
     
-    var button = document.getElementById("counter"),
-    count = 0;
-    button.onclick = function() {
-    count += 1;
-    button.innerHTML = "Click me: " + count;
+    var count = 0;
+    
+    function bcounter(){
+        if(button = document.getElementById("counter"),
+        count = 0;
+        button.onclick = function() {
+        count += 1;
+        button.innerHTML = "Forward: " + count;
+
+        )
+
+    };
+    var 
     };
 
+
+    var button = document.getElementById("counterneg"),
+    count = 0;
+    button.onclick = function() {
+    count -= 1;
+    button.innerHTML = "Backward: " + count;
+    };
+
+    // var button = document.getElementById("counter"),
+    // count = 0;
+    // button.onclick = function() {
+    // count += 1;
+    // button.innerHTML = "Forward: " + count;
+    // };
+
+
+    // var button = document.getElementById("counterneg"),
+    // count = 0;
+    // button.onclick = function() {
+    // count -= 1;
+    // button.innerHTML = "Backward: " + count;
+    // };
+
     $('button').click(function(){  
-    document.getElementById("poke").src = "https://pokeapi.co/media/img/"+count+".png";
+    document.getElementById("poke").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+count+".png";
     }) 
 })
