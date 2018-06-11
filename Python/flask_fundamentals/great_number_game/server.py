71752a2c5d5deae561865367d75626ebec712248
Tom Reese III <tr3limited@gmail.com>
2018-06-11 17:05:22 -0500

    flask_fundamentals
    great_number_game finished

--- Python/flask_fundamentals/great_number_game/server.py
+++ Python/flask_fundamentals/great_number_game/server.py
@@ -6,16 +6,25 @@ app.secret_key='123'
 @app.route('/')
 def index():
     reset=""
-    test=session['box_type']
+    # test=session['box_type']
     if 'result' not in session:
         session['result'] = ""
+    if 'display' not in session:
+        session['display'] = ""
+        print(session['display'])
+    if 'hide' not in session:
+        session['hide'] = "display:none"
     if 'number' not in session:
         result = ""
         session['number'] = str(random.randrange(0,101))
         print("the number is",session['number'])
     else:
         result = session['result']
-    return render_template('index.html',result=result, reset=reset,test=test)
+    if 'style' not in session:
+        session['style'] = ""
+    print(session['display'])
+    
+    return render_template('index.html',result=result, reset=reset,style=session['style'], display=session['display'], hide=session['hide'])
 
 @app.route('/guess',methods=['POST'])
 def guess():
@@ -23,20 +32,26 @@ def guess():
     session['number'] = int(session['number'])
     if session['guess'] == session['number']:
         print("user guessed the correct number")
+        session['style'] = "nailedit"
         session['result'] = 'Great job!  You nailed it!'
-        session['box_type'] = "display:flex;position;relative:background-color:green;font-size:3em;text-align:center;"
+        session['display'] = "display:none;"
+        session['hide'] = ""
         return redirect('/',)
     if session['guess'] > session['number']:
         session['result'] = "Too High!"
-        session['box_type'] = "display:flex;position;relative:background-color:red;font-size:3em;text-align:center;"
+        session['style'] = "toohigh"
         print(session['result'])
-        session.pop('guess')
         return redirect('/')
     if session['guess'] < session['number']:
         session['result'] = "Too Low!"
+        session['style'] = "toolow"
         print(session['result'])
-        session['box_type'] = "display:flex;position;relative:background-color:red;font-size:3em;text-align:center;"
-        session.pop('guess')
     return redirect('/')
+
+@app.route('/reset',methods=['POST'])
+def reset():
+    session.clear()
+    return redirect('/')
+
 
 app.run(debug=True)
