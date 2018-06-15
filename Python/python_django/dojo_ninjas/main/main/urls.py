20909a7358738b5ffdcbbce3b5a8fba1c1ee6670
Tom Reese III <tr3limited@gmail.com>
2018-06-15 18:36:34 -0500

    django_update
    book_authors finished

--- Python/python_django/dojo_ninjas/main/main/urls.py
+++ Python/python_django/dojo_ninjas/main/main/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.dojo_ninjas.urls')),
+    url(r'^book_authors/', include('apps.book_authors.urls')),
 ]
