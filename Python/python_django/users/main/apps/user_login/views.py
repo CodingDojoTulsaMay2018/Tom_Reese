e4bfdd2c59d0e9cfe5f0532e7fd068209c0994df
Tom Reese III <tr3limited@gmail.com>
2018-06-25 15:55:26 -0500

    ajax_update
    ajax_demo finished...

--- Python/python_django/users/main/apps/user_login/views.py
+++ Python/python_django/users/main/apps/user_login/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages 
+from .models import *
+from django.core import serializers
 # Create your views here.
 
 
 def index(request):
-    pass
+    return render(request,"user_login/index.html")
+
+def all_json(request):
+    users = User.objects.all()
+    return HttpResponse(serializers.serialize("json", users), content_type='application/json')
+
+def all_html(request): 
+    return render(request, "user_login/all.html", {'users':User.objects.all()})
+
+def find(request):
+    if not request.POST['first_name_starts_with']:
+        return HttpResponse('awaiting input...')
+    return render(request, "user_login/all.html", {'users':User.objects.filter(first_name__startswith=request.POST['first_name_starts_with'])})
+
+def create(request):
+    User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], age=request.POST['age'],email_address=request.POST['email'])
+    return render(request, "user_login/all.html", {'users':User.objects.order_by('-id')})
