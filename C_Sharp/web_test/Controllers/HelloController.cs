e57bfb2454ca0233f1c79c836bb522f5f8a31e32
Tom Reese III <tr3limited@gmail.com>
2018-07-05 14:47:03 -0500

    C#_Update
    portfolio_finished

---
+++ C_Sharp/web_test/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+    namespace web_test.Controllers     //be sure to use your own project's namespace!
+    {
+        public class HelloController : Controller   //remember inheritance??
+        {
+            //for each route this controller is to handle:
+            [HttpGet]       //type of request
+            [Route("")]     //associated route string (exclude the leading /)
+            public string Index()
+            {
+                return "This is my index!";
+            }
+
+            [HttpGet]       //type of request
+            [Route("projects")]     //associated route string (exclude the leading /)
+            public string Projects()
+            {
+                return "These are my projects!";
+            }
+
+            [HttpGet]       //type of request
+            [Route("contact")]     //associated route string (exclude the leading /)
+            public string Contact()
+            {
+                return "This is my contact!";
+            }
+
+
+        }
+    }
