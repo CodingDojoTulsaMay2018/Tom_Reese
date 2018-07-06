3830da5c1934cd4f62370513ca58c4e88b0b762f
Tom Reese III <tr3limited@gmail.com>
2018-07-06 17:18:05 -0500

    C#_Update
    Dojo Survey Models finished.
    Buggy stuff is crap :pig:

---
+++ C_Sharp/dojo_model_survey/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojo_model_survey.Models;
+
+namespace dojo_model_survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            System.Console.WriteLine("************5refghtrfd****************");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost("Result")]
+
+        public IActionResult Result(string name,string location,string language, string comment)
+        {
+            ViewData["Message"] = "Your result page.";
+            
+            Ninja tom = new Ninja(){
+            Name = name,
+            Location = location,
+            Language = language,
+            Comment = comment,
+            };
+      
+            return View(tom);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C_Sharp/dojo_model_survey/Models/Dojo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+    
+    namespace dojo_model_survey.Models
+    {
+
+
+
+
+public class Ninja
+{
+     public string Name {get;set;}
+     public string Location {get;set;}
+     public string Language {get;set;}
+     public string Comment {get;set;}
+}
+
+    }
