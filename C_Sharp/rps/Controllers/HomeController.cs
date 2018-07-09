345de4439afd2a7345732f9a44ae56b7b25b5c65
Tom Reese III <tr3limited@gmail.com>
2018-07-09 15:52:41 -0500

    C#_Update
    Rock,Paper,Scissors....all options finished.

--- C_Sharp/rps/Controllers/HomeController.cs
+++ C_Sharp/rps/Controllers/HomeController.cs
@@ -28,63 +28,82 @@ namespace rps.Controllers
         {
             Random rand = new Random();
             int com = rand.Next(0,3);
-
             if(com == 0 && Request.Form["hand"] == "rock"){
                 var ties = HttpContext.Session.GetInt32("ties");
                 ties++;
+                HttpContext.Session.SetString("result","it's a TIE!");
+                HttpContext.Session.SetString("com","Rock");
                 HttpContext.Session.SetInt32("ties", (int)ties);
             }
                        if(com == 1 && Request.Form["hand"] == "rock"){
                 var losses = HttpContext.Session.GetInt32("losses");
                 losses++;
+                HttpContext.Session.SetString("result","you LOSE!");
+                HttpContext.Session.SetString("com","Paper");
                 HttpContext.Session.SetInt32("losses", (int)losses);
             }
                        if(com == 2 && Request.Form["hand"] == "rock"){
                 var wins = HttpContext.Session.GetInt32("wins");
                 wins++;
+                HttpContext.Session.SetString("result","you win!");
+                HttpContext.Session.SetString("com","Scissors");
                 HttpContext.Session.SetInt32("wins", (int)wins);
             }
                         if(com == 1 && Request.Form["hand"] == "paper"){
                 var ties = HttpContext.Session.GetInt32("ties");
                 ties++;
+                HttpContext.Session.SetString("result","it's a TIE!");
+                HttpContext.Session.SetString("com","Paper");
                 HttpContext.Session.SetInt32("ties", (int)ties);
             }
                        if(com == 2 && Request.Form["hand"] == "paper"){
                 var losses = HttpContext.Session.GetInt32("losses");
                 losses++;
+                HttpContext.Session.SetString("result","you LOSE!");
+                HttpContext.Session.SetString("com","Scissors");
                 HttpContext.Session.SetInt32("losses", (int)losses);
             }
                        if(com == 0 && Request.Form["hand"] == "paper"){
                 var wins = HttpContext.Session.GetInt32("wins");
                 wins++;
+                HttpContext.Session.SetString("result","you win!");
+                HttpContext.Session.SetString("com","Rock");
                 HttpContext.Session.SetInt32("wins", (int)wins);
             }
                         if(com == 2 && Request.Form["hand"] == "scissors"){
                 var ties = HttpContext.Session.GetInt32("ties");
                 ties++;
+                HttpContext.Session.SetString("result","it's a TIE!");
+                HttpContext.Session.SetString("com","Scissors");
                 HttpContext.Session.SetInt32("ties", (int)ties);
             }
                        if(com == 0 && Request.Form["hand"] == "scissors"){
                 var losses = HttpContext.Session.GetInt32("losses");
                 losses++;
+                HttpContext.Session.SetString("result","you LOSE!");
+                HttpContext.Session.SetString("com","Rock");
                 HttpContext.Session.SetInt32("losses", (int)losses);
             }
                        if(com == 1 && Request.Form["hand"] == "scissors"){
                 var wins = HttpContext.Session.GetInt32("wins");
                 wins++;
+                HttpContext.Session.SetString("result","you win!");
+                HttpContext.Session.SetString("com","Paper");
                 HttpContext.Session.SetInt32("wins", (int)wins);
             }
-            // var result = "";
-            // if(com == 0){
-            //     var result = "Rock";
-            // }
-            // if(com == 1){
-            //     var result = "Paper";
-            // }
-            // if(com == 2){
-            //     var result = "Scissors";
-            // }
-            TempData["com"] = com;
+            if((string)HttpContext.Session.GetString("result") =="you win!") {
+                TempData["color"] = "green";
+            }
+             if((string)HttpContext.Session.GetString("result") =="it's a TIE!") {
+                TempData["color"] = "yellow";
+            }
+             if((string)HttpContext.Session.GetString("result") =="you LOSE!") {
+                TempData["color"] = "red";
+            }
+            
+            TempData["result"] = (string)HttpContext.Session.GetString("result");
+            System.Console.WriteLine(TempData["color"]);
+            TempData["com"] = HttpContext.Session.GetString("com");;
             TempData["you"] = (string)Request.Form["hand"];
             return RedirectToAction("Index");
         }
