22002b75f06128d6917d86706f251f69ab5fe931
Tom Reese III <tr3limited@gmail.com>
2018-07-09 15:14:37 -0500

    C#_Update
    Rock, Paper, Scissors....finished

--- C_Sharp/dojodachi/Controllers/HomeController.cs
+++ C_Sharp/dojodachi/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using dojodachi.Models;
+using System.Web.SessionState;
 
 namespace dojodachi.Controllers
 {
@@ -19,7 +20,13 @@ namespace dojodachi.Controllers
    
      public IActionResult Index()
     {
-                
+        if(HttpContext.Session.GetString("img") == null) {
+        HttpContext.Session.SetString("img", "smile");
+        TempData["msg"] = "Let's play a new game!";
+        }   
+
+        TempData["img"] = HttpContext.Session.GetString("img");
+
         if(HttpContext.Session.GetInt32("fullness") == null)
         {
             HttpContext.Session.SetInt32("fullness", 20);
@@ -27,54 +34,121 @@ namespace dojodachi.Controllers
             HttpContext.Session.SetInt32("meals", 3);
             HttpContext.Session.SetInt32("energy", 50);
         } 
+
         pet.Happiness = (int)HttpContext.Session.GetInt32("happiness");
         pet.Energy = (int)HttpContext.Session.GetInt32("energy");
         pet.Fullness = (int)HttpContext.Session.GetInt32("fullness");
         pet.Meals = (int)HttpContext.Session.GetInt32("meals");          
-        TempData["img"]  = "~/images/smile.png";
+
+        if(HttpContext.Session.GetInt32("meals") == 0 || HttpContext.Session.GetInt32("energy") == 0)
+        {TempData["img"] = "dead";
+        TempData["msg"] = "Smiley is dead! You killed it, you monster!";
+        return View(pet);
+        }
         return View(pet);
     }
+
 
         public IActionResult Feed()
         {
 
         int mealsleft = (int)HttpContext.Session.GetInt32("meals");
-
-        if(mealsleft > 0)
-        {
+        var chance = rand.Next(0,4);
+        if(chance == 0){
+        mealsleft--;
+        HttpContext.Session.SetInt32("meals", mealsleft);
+        TempData["msg"] = "You fed Smiley, but he threw it up! Meals: -1";
+        return RedirectToAction("Index");
+        }
+        else{
         var addFullness = rand.Next(5,11);
         int currentFullness = (int)HttpContext.Session.GetInt32("fullness");
         currentFullness += addFullness;
         HttpContext.Session.SetInt32("fullness", currentFullness);
         mealsleft--;
         HttpContext.Session.SetInt32("meals", mealsleft);
-        }   
-        TempData["msg"] = "You fed Smiley!";
+        HttpContext.Session.SetString("img", "smile");
+        TempData["img"]  = HttpContext.Session.GetString("img");
+        }
+
+        HttpContext.Session.SetString("img", "smile");
+        TempData["img"]  = HttpContext.Session.GetString("img");
+        TempData["msg"] = ("You fed Smiley! Meals: -1, Fullness: +"+addFullness);
         TempData["full"] = HttpContext.Session.GetInt32("fullness");
         TempData["happ"] = HttpContext.Session.GetInt32("happiness");
         TempData["meal"] = HttpContext.Session.GetInt32("meals");
         TempData["ener"] = HttpContext.Session.GetInt32("energy");                 
-        // TempData["img"]  = (<img src='~/images/smile.png'>);
         return RedirectToAction("Index");
         }
 
+
         public IActionResult Play()
         {
-            TempData["msg"] = "You played with Smiley!";
+        var chance = rand.Next(0,4);
+        if(chance == 0){
+            int energyLeft = (int)HttpContext.Session.GetInt32("energy");
+            energyLeft -= 5;
+            HttpContext.Session.SetInt32("energy", energyLeft);
+            TempData["msg"] = "You played with Smiley, but he didn't enjoy it! Energy: -5";
+        }
+        else{
+        var addHappy = rand.Next(5,11);
+        int currentHappy = (int)HttpContext.Session.GetInt32("happiness");
+        currentHappy += addHappy;
+        HttpContext.Session.SetInt32("happiness", currentHappy);
+        int energyLeft = (int)HttpContext.Session.GetInt32("energy");
+        energyLeft -= 5;
+        HttpContext.Session.SetInt32("energy", energyLeft);
+        TempData["msg"] = "You played with Smiley! Energy: -5, Happiness: "+addHappy;
+        }
+
+        if(HttpContext.Session.GetInt32("energy") <= 0){
+            TempData["msg"] = "Smiley is dead! You killed it, you monster!";
+            HttpContext.Session.SetString("img", "dead");
+
+            return RedirectToAction("Index");
+        }
+      
+        
+        TempData["happ"] = HttpContext.Session.GetInt32("happiness");
+        TempData["ener"] = HttpContext.Session.GetInt32("energy");                 
+        HttpContext.Session.SetString("img", "smile");
 
             return RedirectToAction("Index");
         }
 
         public IActionResult Work()
         {
-            TempData["msg"] = "You went to work!";
+
+        var chance = rand.Next(1,4);
+        int energyLeft = (int)HttpContext.Session.GetInt32("energy");
+        energyLeft -= 5;
+        HttpContext.Session.SetInt32("energy", energyLeft); 
+        var mealsleft = (int)HttpContext.Session.GetInt32("meals");
+        mealsleft += chance;
+        HttpContext.Session.SetInt32("meals", mealsleft);                
+        HttpContext.Session.SetString("img", "smile");
+        TempData["msg"] = ("You went to work! Evergy: -5, Meals: +"+chance);
 
             return RedirectToAction("Index");
         }
 
         public IActionResult Sleep()
         {
-            TempData["msg"] = "You went to sleep!";
+            int energyLeft = (int)HttpContext.Session.GetInt32("energy");
+            energyLeft += 15;
+            HttpContext.Session.SetInt32("energy", energyLeft); 
+
+            int currentFullness = (int)HttpContext.Session.GetInt32("fullness");
+            currentFullness -= 5;
+            HttpContext.Session.SetInt32("fullness", currentFullness);
+            int currentHappy = (int)HttpContext.Session.GetInt32("happiness");
+            currentHappy -= 5;
+            HttpContext.Session.SetInt32("happiness", currentHappy);
+
+            HttpContext.Session.SetString("img", "smile");
+            TempData["msg"] = ("You went to sleep! Evergy: +5, Happiness -5, Fullness: -5");
+
             return RedirectToAction("Index");
         }
 
---
+++ C_Sharp/rps/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace rps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+        if(HttpContext.Session.GetInt32("wins") == null || HttpContext.Session.GetInt32("losses") == null || HttpContext.Session.GetInt32("ties") == null) {
+        HttpContext.Session.SetInt32("wins", 0);
+        HttpContext.Session.SetInt32("ties", 0);
+        HttpContext.Session.SetInt32("losses", 0);
+        }
+        TempData["wins"] = HttpContext.Session.GetInt32("wins");
+        TempData["losses"] = HttpContext.Session.GetInt32("losses");
+        TempData["ties"] = HttpContext.Session.GetInt32("ties");
+                return View();
+
+        }
+
+        public IActionResult Process_Play()
+        {
+            Random rand = new Random();
+            int com = rand.Next(0,3);
+
+            if(com == 0 && Request.Form["hand"] == "rock"){
+                var ties = HttpContext.Session.GetInt32("ties");
+                ties++;
+                HttpContext.Session.SetInt32("ties", (int)ties);
+            }
+                       if(com == 1 && Request.Form["hand"] == "rock"){
+                var losses = HttpContext.Session.GetInt32("losses");
+                losses++;
+                HttpContext.Session.SetInt32("losses", (int)losses);
+            }
+                       if(com == 2 && Request.Form["hand"] == "rock"){
+                var wins = HttpContext.Session.GetInt32("wins");
+                wins++;
+                HttpContext.Session.SetInt32("wins", (int)wins);
+            }
+                        if(com == 1 && Request.Form["hand"] == "paper"){
+                var ties = HttpContext.Session.GetInt32("ties");
+                ties++;
+                HttpContext.Session.SetInt32("ties", (int)ties);
+            }
+                       if(com == 2 && Request.Form["hand"] == "paper"){
+                var losses = HttpContext.Session.GetInt32("losses");
+                losses++;
+                HttpContext.Session.SetInt32("losses", (int)losses);
+            }
+                       if(com == 0 && Request.Form["hand"] == "paper"){
+                var wins = HttpContext.Session.GetInt32("wins");
+                wins++;
+                HttpContext.Session.SetInt32("wins", (int)wins);
+            }
+                        if(com == 2 && Request.Form["hand"] == "scissors"){
+                var ties = HttpContext.Session.GetInt32("ties");
+                ties++;
+                HttpContext.Session.SetInt32("ties", (int)ties);
+            }
+                       if(com == 0 && Request.Form["hand"] == "scissors"){
+                var losses = HttpContext.Session.GetInt32("losses");
+                losses++;
+                HttpContext.Session.SetInt32("losses", (int)losses);
+            }
+                       if(com == 1 && Request.Form["hand"] == "scissors"){
+                var wins = HttpContext.Session.GetInt32("wins");
+                wins++;
+                HttpContext.Session.SetInt32("wins", (int)wins);
+            }
+            // var result = "";
+            // if(com == 0){
+            //     var result = "Rock";
+            // }
+            // if(com == 1){
+            //     var result = "Paper";
+            // }
+            // if(com == 2){
+            //     var result = "Scissors";
+            // }
+            TempData["com"] = com;
+            TempData["you"] = (string)Request.Form["hand"];
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
