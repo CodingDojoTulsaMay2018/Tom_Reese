7423142939bbdf7a8ee5af8d57bb2bb7cec8587f
Tom Reese III <tr3limited@gmail.com>
2018-07-17 15:17:28 -0500

    C# Update
    Dojo Secrets finishd

--- C_Sharp/dojosecrets/Controllers/HomeController.cs
+++ C_Sharp/dojosecrets/Controllers/HomeController.cs
@@ -38,6 +38,9 @@ namespace dojosecrets.Controllers
 
         public IActionResult Secrets()
         {
+            if(HttpContext.Session.GetInt32("id") == null) { 
+                return RedirectToAction("Logout");}
+
             ViewBag.name = HttpContext.Session.GetString("name");
             ViewBag.id = HttpContext.Session.GetInt32("id");
             List<Secret> AllSecrets = _context.secrets.Include(u => u.Likes).ThenInclude(g => g.Liked).ToList();
@@ -45,7 +48,7 @@ namespace dojosecrets.Controllers
               i.Ago = i.Created_At.TimeAgo();
             }
             ViewBag.secrets = AllSecrets;
-
+            TempData["secrets"] = "secrets";
             return View();
         }
 
@@ -96,7 +99,15 @@ namespace dojosecrets.Controllers
 
         public IActionResult Popular()
         {
-
+            if(HttpContext.Session.GetInt32("id") == null) { 
+                return RedirectToAction("Logout");};
+            ViewBag.name = HttpContext.Session.GetString("name");
+            ViewBag.id = HttpContext.Session.GetInt32("id");
+            List<Secret> AllSecrets = _context.secrets.Include(u => u.Likes).ThenInclude(g => g.Liked).ToList();
+            foreach(var i in AllSecrets){
+              i.Ago = i.Created_At.TimeAgo();
+            }
+            ViewBag.secrets = AllSecrets;
 
             return View();
         }
@@ -106,6 +117,8 @@ namespace dojosecrets.Controllers
         [HttpPost]
         public IActionResult New(Secret FormData)
         {
+                           if(HttpContext.Session.GetInt32("id") == null) { 
+                return RedirectToAction("Logout");};
             if(ModelState.IsValid){
             User user = _context.users.SingleOrDefault(u => u.Id== HttpContext.Session.GetInt32("id"));
             FormData.Creator = user;
@@ -118,22 +131,54 @@ namespace dojosecrets.Controllers
             return View("Secrets");
         }
 
+        [HttpGet]
+        [Route("Home/Like/{num}/{word}")]
+        public IActionResult Like(int num, string word)
+        {
+            if(HttpContext.Session.GetInt32("id") == null) { 
+                return RedirectToAction("Logout");};
+
+            Secret it = _context.secrets.Include(g => g.Likes).ThenInclude(a => a.Liked).SingleOrDefault(u => u.Id == num);
+            User user = _context.users.SingleOrDefault(u => u.Id== HttpContext.Session.GetInt32("id"));
+
+            Like newl = new Like{
+                Userid = user.Id,
+                Liked = user,
+                Secretid = it.Id,
+                Haslikes = it
+            };
+
+            it.Likes.Add(newl);
+            _context.SaveChanges();
+            if(word == "secrets"){
+            return RedirectToAction("Secrets");
+            }
+            return RedirectToAction("Popular");
+        }
+
+        [HttpGet]
+        [Route("Home/Delete/{num}/{word}")]
+        public IActionResult Delete(int num, string word)
+        {
+               if(HttpContext.Session.GetInt32("id") == null) { 
+                return RedirectToAction("Logout");};
+
+            Secret del = _context.secrets.SingleOrDefault(u => u.Id == num);
+            _context.Remove(del);
+            _context.SaveChanges();
+            if(word == "secrets"){
+            return RedirectToAction("Secrets");
+            }
+            return RedirectToAction("Popular");
+        }
 
 
-
-    //         public IActionResult Logout()
-    //     {
-    //         HttpContext.Session.Clear();
-    //         System.Console.WriteLine("***User is logging out ***");
-    //         return RedirectToAction("Index", "Home");
-    //     }
+            public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            System.Console.WriteLine("***User is logging out ***");
+            return RedirectToAction("Index");
+        }
     
-
-
-
-    //     public IActionResult Error()
-    //     {
-    //         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
     }
 }
