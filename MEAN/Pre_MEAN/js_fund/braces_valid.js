482bca14e51bbca54aeca60e4e63605b61bd1cd4
Tom Reese III <tr3limited@gmail.com>
2018-07-29 21:55:45 -0500

    mean_update
    braces valid finished

--- MEAN/Pre_MEAN/js_fund/braces_valid.js
+++ MEAN/Pre_MEAN/js_fund/braces_valid.js
@@ -1,9 +1,40 @@
-function bracesvalid(string){
-var key = {};
-
-
+function bracesvalid(str){
+    let check = [];
+    let key = {
+        "}" : "{",
+        "]" : "[",
+        ")" : "("};
+        for(let i = 0; i < str.length; i++){
+            if(str[i] === "{" ||str[i] === "[" ||str[i] === "("){
+                check.unshift(str[i])
+            }            
+            if(str[i] === "}"){
+                if((check[0]) == key["}"]){
+                    check.shift();
+                    continue;}
+                return false;
+            }
+            if(str[i] === "]"){
+                if((check[0]) == key["]"]){
+                    check.shift();
+                    continue;}
+                return false;
+            }
+            if(str[i] === ")"){
+                if((check[0]) == key[")"]){
+                    check.shift();
+                    continue;}
+                return false;
+            }
+        }
+if(check.length){
+    return false;
 }
+return true
+};
 
 
 
-bracesvalid("rty{tyu[rty(erty)erty}rt].")
+
+console.log(bracesvalid("rty{tyu[rty(erty)erty]rt}."))
+console.log(bracesvalid("}rty{tyu}{rty(erty)erty]rt}."))
