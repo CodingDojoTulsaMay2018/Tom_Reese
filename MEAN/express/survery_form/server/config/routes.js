350e2b612f8f00e1581dee7cc648c2dfc8a15469
Tom Reese III <tr3limited@gmail.com>
2018-08-02 16:46:56 -0500

    mean_update
    survery form finished

---
+++ MEAN/express/survery_form/server/config/routes.js
@@ -0,0 +1,23 @@
+module.exports = app =>{
+  app.get("/", (req, res)=> {
+
+  res.render('index')})
+
+  app.post("/submit", (req, res)=> {
+  console.log("at process");
+  req.session.name = req.body.name;
+  req.session.dojo_location = req.body.dojo_location;
+  req.session.fav_lang = req.body.fav_lang;
+  req.session.comment = req.body.comment;
+
+
+  res.redirect('results')})
+
+  app.get("/results", (req, res)=> {
+  console.log("at result");
+  
+    res.render('results', {session : req.session})})
+
+}
+
+
