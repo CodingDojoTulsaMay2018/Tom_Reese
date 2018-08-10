8c34665735db49c2eb0b86e0af1bf174e2f94cd7
Tom Reese III <tr3limited@gmail.com>
2018-08-10 11:59:28 -0500

    mean_update
    pokemon api finished

---
+++ MEAN/angular/pokemon_api/public/src/app/http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private _http: HttpClient) {
+    this.getPoke();
+   }
+
+
+   getPoke(){
+     let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/143/');
+     bulbasaur.subscribe(data => console.log(data))
+     bulbasaur.subscribe(data => {for(var i of data["abilities"]){console.log(i.ability.name);}})
+
+     let allpokes = this._http.get('https://pokeapi.co/api/v2/ability/immunity/');
+     allpokes.subscribe(data => {for(var i of data["pokemon"]){console.log(i.pokemon["name"])}})
+    
+  };
+
+}
+  
+
